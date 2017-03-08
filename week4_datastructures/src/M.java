95cebd83d1555830645ffee46bdfde23f26434e8
Troels Just Christoffersen <troelsjc@gmail.com>
2017-03-08 19:36:00 +0100

    w4

---
+++ week4_datastructures/src/M.java
@@ -0,0 +1,24 @@
+/**
+ * Created by troels on 2017-03-08.
+ *
+ * only a sketch
+ */
+public class M {
+    public static void main(String[] args) {
+        insertionSort();
+    }
+
+    private static int insertionSort(A) {
+        for (int j = 0; j < A.length; j += 5000) {
+            System.out.println(j);
+        }
+        return 0;
+    }
+
+    private static int[] randomIntArray() {
+        aList[];
+        for (int j = 0; j < 75000; j += 5000) {
+            System.out.println(j);
+        }
+    }
+}
